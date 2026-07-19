59a0c0ddf4061c0fb207459daee3abff83f23a0b
Piero <pierevco@gmail.com>
2026-07-19 15:51:56 -0400

    CUDA Spheres

--- main.cu
+++ main.cu
@@ -6,11 +6,49 @@
 #include "vec3.h"
 #include "ray.h"
 
+struct Sphere { Vector3 center; float radius; };
+struct HitRecord { Vector3 p; Vector3 normal; float t; bool front_face; };
 
+__device__ bool hit_sphere(const Sphere& s, const Ray& r, float t_min, float t_max, HitRecord& rec) {
+    Vector3 oc = s.center - r.origin;
+    float a = length_squared(r.direction);
+    float h = dot(r.direction, oc);
+    float c = length_squared(oc) - s.radius*s.radius;
+    float disc = h*h - a*c;
+    if (disc < 0) return false;
+    float sqrtd = sqrtf(disc);
+    float root = (h - sqrtd)/a;
+    if (root <= t_min || root >= t_max){
+        root = (h + sqrtd)/a;
+        if (root <= t_min || root >= t_max){
+            return false;
+        }
+    }
+    rec.t = root;
+    rec.p = r.at(rec.t);
+    Vector3 outward = (rec.p - s.center) / s.radius;
+    rec.front_face = dot(r.direction, outward) < 0.0f;
+    rec.normal = rec.front_face ? outward : -outward;
+    return true;
+}
 
+__device__ bool hit_world(const Sphere* world, int n, const Ray& r, float t_min, float t_max, HitRecord& rec) {
+    HitRecord temp_rec;
+    bool hit_anything = false;
+    float closest_so_far = t_max;
 
+    for (int k = 0; k < n; k++){
+        if (hit_sphere(world[k], r, t_min, closest_so_far, temp_rec)){
+            hit_anything = true;
+            closest_so_far = temp_rec.t;
+            rec = temp_rec;
+        }
+    }
+    return hit_anything;
 
-__global__ void render (float* fb, int width, int height){
+}
+
+__global__ void render (float* fb, Sphere* world, int n, int width, int height){
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     int j = threadIdx.y + blockIdx.y * blockDim.y;
     if (i >= width || j >= height) return;
@@ -41,12 +79,21 @@ __global__ void render (float* fb, int width, int height){
     Vector3 viewport_upper_left = center - focus_dist * w - viewport_u / 2.0f - viewport_v / 2.0f;
     pixel00_loc = 0.5f * (pixel_delta_u + pixel_delta_v) +  viewport_upper_left;
 
-    Vector3 ray_origin = center + defocus_disk_u + defocus_disk_v; //needs curand
+    Vector3 pixel_color;
     Vector3 sample = pixel00_loc + i * pixel_delta_u + j * pixel_delta_v;
-    Ray r = Ray(ray_origin, sample - ray_origin);
-    Vector3 unit_direction = unit(r.direction);
-    float a = 0.5f * (unit_direction.y + 1.0f);
-    Vector3 pixel_color = (1.0f - a) * Vector3(1.0f,1.0f,1.0f) + a * Vector3(0.5f, 0.7f, 1.0f); 
+    Ray r = Ray(center, sample - center);
+
+    HitRecord rec;
+    if (hit_world(world, n , r , 0.001f, 1e30f, rec)){
+        //sphere
+        pixel_color = 0.5f * (rec.normal + Vector3(1,1,1));
+    }
+    else{
+        //sky
+        Vector3 unit_direction = unit(r.direction);
+        float a = 0.5f * (unit_direction.y + 1.0f);
+        pixel_color = (1.0f - a) * Vector3(1.0f,1.0f,1.0f) + a * Vector3(0.5f, 0.7f, 1.0f);
+    } 
 
     int idx = 3 * (j * width + i);
     fb[idx] = pixel_color.x;
@@ -61,14 +108,22 @@ int main(){
     float* fb = nullptr;
     int width = 400;
     int height = 225;
+    int n = 4;
+    Sphere* world;
 
+    cudaMallocManaged(&world, n * sizeof(Sphere));
     cudaMallocManaged(&fb, 3*width*height*sizeof(float));
-   
+
+    world[0] = {Vector3(0,-1000,0), 1000};
+    world[1] = {Vector3(0,1,0), 1};
+    world[2] = {Vector3(-4,1,0), 1};
+    world[3] = {Vector3(4,1,0), 1};
+
 
     dim3 blocks(width/8+1, height/8+1);
     dim3 threads(8,8);
 
-    render<<<blocks,threads>>>(fb, width, height);
+    render<<<blocks,threads>>>(fb, world, n,  width, height);
     cudaDeviceSynchronize();
 
     cudaError_t err = cudaGetLastError();
@@ -88,6 +143,7 @@ int main(){
     }
 
     cudaFree(fb);
+    cudaFree(world);
 
     return 0;
 }
